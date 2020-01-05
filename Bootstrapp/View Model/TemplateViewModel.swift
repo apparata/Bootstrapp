1abd0324c83521cf121cd8f131828e75a487d77e
Martin Johannesson <mjohannesson@gmail.com>
2020-01-05 20:31:30 +0100

    Add support for General and General Meta Template categories of templates.

--- Bootstrapp/View Model/TemplateViewModel.swift
+++ Bootstrapp/View Model/TemplateViewModel.swift
@@ -14,10 +14,14 @@ struct TemplateViewModel: Identifiable, Hashable, SearchFilterable {
 
     var icon: String {
         switch template.specification.type {
+        case .general:
+            return "Sidebar Icons/General"
         case .xcodeProject(_):
             return "Sidebar Icons/Xcode Project"
         case .swiftPackage:
             return "Sidebar Icons/Swift Package"
+        case .generalMetaTemplate:
+            return "Sidebar Icons/General Meta Template"
         case .xcodeMetaTemplate:
             return "Sidebar Icons/Xcode Meta Template"
         case .swiftMetaTemplate:
