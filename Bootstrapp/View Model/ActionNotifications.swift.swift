087b253e772fcdbf54c9754bb277a4916410cc00
Martin Johannesson <mjohannesson@gmail.com>
2023-08-20 23:29:02 +0200

    Remove unused code.

--- Bootstrapp/View Model/ActionNotifications.swift.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Copyright © 2021 Apparata AB. All rights reserved.
-//
-
-import Foundation
-
-extension Notification.Name {
-    static let toolbarMakeAction = Self("toolbarMakeActionNotification")
-}
